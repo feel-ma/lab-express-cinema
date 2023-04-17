517683c33bc90c5b52efd19fbaa0f25e6accdce3
Filippo Marinolli <f.marinolli@gmail.com>
2023-04-17 20:27:58 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model.js");
+const db = require("../db/index.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find().then((mdb) => {
+    console.log(mdb);
+    res.render("movies", { mo: mdb });
+  });
+});
+
+router.get("/movies/:id", (req, res) =>{
+    Movie.findById(req.params.id)
+    .then((mov) => {
+        res.render('movieD', {mov})
+    })
+})
 
 module.exports = router;
